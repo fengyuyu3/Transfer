27cd48855206e4e3bc40378e1005acc4eaa0e4b7
李万汇Will <liwanhui@bestwise.cc>
2017-03-31 18:13:52 +0800

    1、  二维码显示

--- app/src/main/java/com.ironaviation.traveller/mvp/ui/my/QRCodeActivity.java
+++ app/src/main/java/com.ironaviation.traveller/mvp/ui/my/QRCodeActivity.java
@@ -1,11 +1,21 @@
 package com.ironaviation.traveller.mvp.ui.my;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.ironaviation.traveller.R;
 import com.ironaviation.traveller.common.AppComponent;
 import com.ironaviation.traveller.common.WEActivity;
+
+import cn.bingoogolapple.qrcode.core.BGAQRCodeUtil;
+import cn.bingoogolapple.qrcode.zxing.QRCodeEncoder;
 
 /**
  * 项目名称：Traveller
@@ -37,6 +47,39 @@ public class QRCodeActivity extends WEActivity {
 
     @Override
     protected void initData() {
+        createEnglishQRCode("11326");
+        mToolbar.setTitle("订单二维码");
+        mToolbar.setBackgroundColor(ContextCompat.getColor(this, R.color.bg_champagne));
+        setNavigationIcon(ContextCompat.getDrawable(this, R.mipmap.ic_back));
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 
+    private void createEnglishQRCode(final String cord) {
+        /*
+        这里为了偷懒，就没有处理匿名 AsyncTask 内部类导致 Activity 泄漏的问题
+        请开发在使用时自行处理匿名内部类导致Activity内存泄漏的问题，处理方式可参考 https://github.com/GeniusVJR/LearningNotes/blob/master/Part1/Android/Android%E5%86%85%E5%AD%98%E6%B3%84%E6%BC%8F%E6%80%BB%E7%BB%93.md
+         */
+        new AsyncTask<Void, Void, Bitmap>() {
+            @Override
+            protected Bitmap doInBackground(Void... params) {
+
+                return QRCodeEncoder.syncEncodeQRCode(cord, BGAQRCodeUtil.dp2px(QRCodeActivity.this, 150), Color.parseColor("#000000"), BitmapFactory.decodeResource(getResources(), R.mipmap.ic_person));
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                if (bitmap != null) {
+                    ImageView zxingview = (ImageView) findViewById(R.id.zxingview);
+                    zxingview.setImageBitmap(bitmap);
+                } else {
+                    Toast.makeText(QRCodeActivity.this, "生成二维码失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }.execute();
     }
 }
